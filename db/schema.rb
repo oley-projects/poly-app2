ef040d24946eeaabc36e408b83dc63bce1a1d645
Oley-Projects <oley.for.git@gmail.com>
2017-12-27 23:23:16 +0200

    User scaffolding update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227202916) do
+ActiveRecord::Schema.define(version: 20171227212127) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "user"
+    t.text "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "user"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
