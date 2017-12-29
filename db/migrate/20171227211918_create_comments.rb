51026f8b3c710dc219a1e697d4f1f590df9db530
Oley-Projects <oley.for.git@gmail.com>
2017-12-29 23:12:27 +0200

    Polymorphic associations

--- db/migrate/20171227211918_create_comments.rb
+++ db/migrate/20171227211918_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
     create_table :comments do |t|
       t.string :user
       t.text :content
+      t.references :commentable, polymorphic: true
 
       t.timestamps
     end
