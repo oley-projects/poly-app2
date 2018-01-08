d3491ac479661e59f2e558a6cdeed2ab4eadfffd
Oley-Projects <oley.for.git@gmail.com>
2018-01-08 23:52:04 +0200

    One-ty-many Post-Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227212127) do
+ActiveRecord::Schema.define(version: 20180108214150) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user"
@@ -19,7 +19,9 @@ ActiveRecord::Schema.define(version: 20171227212127) do
     t.integer "commentable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "images", force: :cascade do |t|
